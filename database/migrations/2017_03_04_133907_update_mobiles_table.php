e287eabc86c94596b81d4916310ee16742f94fde
Chaudhry Waqas <waqasraza123@gmail.com>
2017-05-14 05:51:18 +0500

    fixed migrations

--- database/migrations/2017_03_04_133907_update_mobiles_table.php
+++ database/migrations/2017_03_04_133907_update_mobiles_table.php
@@ -14,10 +14,10 @@ class UpdateMobilesTable extends Migration
     public function up()
     {
         Schema::disableForeignKeyConstraints();
-
-
         Schema::table('mobiles', function (Blueprint $table) {
             $table->dropColumn(['link', 'old_price', 'current_price', 'discount', 'shop_id', 'local_online', 'stock']);
+            $table->dropForeign('mobiles_shop_id_foreign');
+            $table->dropColumn('shop_id');
             if(!Schema::hasColumn('mobiles', 'storage'))
                 $table->unsignedInteger('storage');
             if(!Schema::hasColumn('mobiles', 'color'))
