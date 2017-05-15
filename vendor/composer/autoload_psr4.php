254002b0ed462f334d2d10b4a5a95d56212acd88
Chaudhry Waqas <waqasraza123@gmail.com>
2017-05-15 07:01:56 +0500

    added pheanstalk queue server

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -32,6 +32,7 @@ return array(
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'Predis\\' => array($vendorDir . '/predis/predis/src'),
     'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
+    'Pheanstalk\\' => array($vendorDir . '/pda/pheanstalk/src'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
     'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
     'Laravel\\Scout\\' => array($vendorDir . '/laravel/scout/src'),
