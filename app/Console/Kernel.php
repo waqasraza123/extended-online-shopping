7eef64ab6c8964d01279f54ed8379cd3f3e7eff7
Chaudhry Waqas <waqasraza123@gmail.com>
2017-05-17 04:25:18 +0500

    exec python code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->exec('/python scripts/gsm.py')->everyMinute();
+        $schedule->exec('/python scripts/gsm.py');
     }
 
     /**
