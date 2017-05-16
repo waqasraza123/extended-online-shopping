dd046e2110207771c64c845ce65963359498081a
Chaudhry Waqas <waqasraza123@gmail.com>
2017-05-17 04:32:31 +0500

    exec python code

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->exec('/python scripts/gsm.py');
+        $schedule->exec(app_path().'/python scripts/gsm.py');
     }
 
     /**
